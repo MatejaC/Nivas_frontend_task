74550fc8ab92e714bc8149b431e1e3e6e9301d99
Mateja Cimpersak <matejacimpersak@gmail.com>
2020-01-31 03:21:48 +0100

    formatting index.html

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,25 +1,5 @@
-document.querySelector('html').clientWidth;
+const navToggle = document.querySelector('.nav__toggle');
 
-class ResponsiveBackgroundImage {
-	constructor(element) {
-		this.element = element;
-		this.img = element.querySelector('img');
-		this.src = '';
-
-		this.img.addEventListener('load', () => {
-			this.update();
-		});
-
-		if (this.img.complete) {
-			this.update();
-		}
-	}
-
-	update() {
-		let src = typeof this.img.currentSrc !== 'undefined' ? this.img.currentSrc : this.img.src;
-		if (this.src !== src) {
-			this.src = src;
-			this.element.style.backgroundImage = 'url("' + this.src + '")';
-		}
-	}
-}
+navToggle.addEventListener('click', () => {
+	document.body.classList.toggle('nav-open');
+});
